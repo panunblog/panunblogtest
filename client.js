37376c33973626a9d2407eb7c23f922c37d49677
panunblog <125153512+panunblog@users.noreply.github.com>
2023-08-08 23:37:21 +0530

    Update client.js

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ import sanityClient from "@sanity/client"
 export default sanityClient({
     projectId: 'uwzkln7p', // you can find this in sanity.json
     dataset: 'production', // or the name you chose in step 1
-    useCdn: true,
+    useCdn: false,
     apiVersion: '2021-08-31' 
   })
 
